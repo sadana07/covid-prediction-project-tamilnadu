aead9a28ed4777ac4ce8c1d19193c5d4a84f6642
sadana07 <sadanasankar2006@gmail.com>
2026-04-27 22:49:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,18 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 app = Flask(__name__)
 CORS(app)   
-# Load data
 district_summary = pd.read_csv("district_summary.csv")
 daily_cases = pd.read_csv("daily_cases.csv")
-# Train ML model
 X = daily_cases[["Day_Number"]]
 y = daily_cases["Hospitalized"]
 model = LinearRegression()
 model.fit(X, y)
-# Home
 @app.route("/")
 def home():
     return "COVID API Running"
-# District data
 @app.route("/districts")
 def districts():
     return jsonify(district_summary.to_dict(orient="records"))
-# Prediction
 @app.route("/predict")
 def predict():
     last_day = daily_cases["Day_Number"].max()
@@ -37,6 +32,5 @@ def predict():
         })
 
     return jsonify(result)
-# Run
 if __name__ == "__main__":
     app.run(debug=True)
